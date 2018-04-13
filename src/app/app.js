eaa3b773e6d6e86003e3edf497c75e747621fa04
karolin siebert <karolin@intellisense.io>
2018-04-13 17:41:29 +0200

    style and initialise fields

--- src/app/app.js
+++ src/app/app.js
@@ -1,25 +1,47 @@
 import angular from 'angular';
+import moment from 'moment';
 
 import '../style/app.css';
 
-let app = () => {
-  return {
-    template: require('./app.html'),
-    controller: 'AppCtrl',
-    controllerAs: 'app'
-  }
-};
+angular.module('app', [])
+  .directive('datePicker', () => {
+    function link ($scope){
+        $scope.allDays = [1, 2, 4, 5, 13];
+        $scope.allMonths = [1, 12, 10, 5, 4];
+        $scope.chosenYear = undefined;
 
-class AppCtrl {
-  constructor() {
-    this.url = 'https://github.com/preboot/angular-webpack';
-  }
-}
+        $scope.updateDay = (day) => {
+          console.log(day);
+          $scope.currentDate = moment($scope.currentDate).day(day);
+        };
 
-const MODULE_NAME = 'app';
+        $scope.updateMonth = (month) => {
+          console.log(month);
+          $scope.currentDate = moment($scope.currentDate).month(month);
+        };
 
-angular.module(MODULE_NAME, [])
-  .directive('app', app)
-  .controller('AppCtrl', AppCtrl);
+        $scope.updateYear = (year) => {
+          console.log(year);
+          $scope.currentDate = moment($scope.currentDate).year(year);
+        };
 
-export default MODULE_NAME;
+       $scope.formatDate = (date) => {
+          return moment(date).format("MMMM Do YYYY");
+        };
+
+        //we are always initialising to current date
+        $scope.currentDate = moment(new Date());
+
+        $scope.selectedYear = $scope.currentDate.year();
+        $scope.selectedDay = $scope.currentDate.day().toString();
+        $scope.selectedMonth =  $scope.currentDate.month().toString();
+
+        console.log($scope.selectedYear, $scope.selectedMonth, $scope.selectedDay);
+    }
+    return {
+      template: require('./views/date-picker.directive.view.html'),
+      link: link
+    };
+  });
+
+export default 'app';
